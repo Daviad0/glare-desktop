5a891557271bf51dc5167d39dffbb34a71f5227c
David Reeves <djreeves2005@gmail.com>
2021-06-20 12:32:23 -0400

    initialization of the desktop app

---
+++ app.js
@@ -0,0 +1,115 @@
+/*
+  Title: ElectronJS
+  Author: OpenJS Foundation
+  Date: 2/12/2021
+  Code Version: 4.2.12
+  Code Availaibility: https://www.electronjs.org/
+  Notes: This is the library that brings the HTML page to life in a new window instead of the Chrome browser. All content that shows up inside the window is not within Electron's control, and is completely coded by this project's author
+*/
+const {app, BrowserWindow, dialog} = require('electron')
+const {ipcMain} = require('electron')
+// Defines where to grab the already available audio files.
+
+
+/*
+  Title: FS (File System)
+  Author: NPM Js
+  Date: 2/12/2021
+  Code Version: Built in w/ NodeJS
+  Code Availaibility: https://nodejs.org/en/
+*/
+const fs = require('fs')
+
+
+
+
+//resetServerMem();
+
+
+/*
+  Title: Socket.IO (Client)
+  Author: rauchg
+  Date: 2/12/2021
+  Code Version: 3.1.1
+  Code Availaibility: https://github.com/socketio/socket.io-client
+  Notes: Paired with the Socket.IO package on app.js through port 3000
+*/
+const io = require("socket.io-client")
+/*
+  Title: Request
+  Author: fredkschott
+  Date: 2/12/2021
+  Code Version: 2.88.2
+  Code Availaibility: https://www.npmjs.com/package/request
+  Notes: Depreciated, but still used
+*/
+const request = require('request')
+/*
+  Title: node-machine-id
+  Author: automation-stack
+  Date: 2/12/2021
+  Code Version: 1.1.12
+  Code Availaibility: https://www.npmjs.com/package/node-machine-id
+*/
+const machineId = require('node-machine-id')
+
+
+
+// main window that actually allows the user to interact with the show
+function createWindow () {
+
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }, 
+    title : 'Glare Desktop',
+    show: false
+    
+  })
+  //mainWindow.setFullScreen(true)
+
+  var fileToLoad = "views/server.html"
+  mainWindow.loadFile(fileToLoad)
+  mainWindow.isMenuBarVisible(false)
+  
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  });
+  mainWindow.webContents.on('did-finish-load', function() {
+    mainWindow.show();
+    try{
+      roleSelectionWindow.close();
+    }catch(err){
+      
+    }
+    
+  });
+  
+  
+}
+
+
+
+// when ElectronJS is ready, start up the role selection window
+app.on('ready', createWindow)
+
+// event for user resize (if allowed by the window definition)
+app.on('resize', function(e,x,y){
+  mainWindow.setSize(x, y);
+});
+
+// quit when every window is closed, except if on macintosh (due to library issues)
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+// if there is no mainWindow on activation (other than the first runtime), start up the role window
+app.on('activate', function () {
+  if (mainWindow === null) {
+    letUserSelectRole()
+  }
+})

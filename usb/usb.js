6c252ad3f6ae9dbdaf7884bd7769d88b5c2df321
David Reeves <djreeves2005@gmail.com>
2022-04-11 08:08:56 -0400

    have a test for basic usb initialization

--- usb/usb.js
+++ usb/usb.js
@@ -4,6 +4,25 @@ const { bmRequestType, DIRECTION, TYPE, RECIPIENT } = require('bmrequesttype');
 
 var UsedDevice = undefined
 
+
+
+function trySendData(phaseModel){
+  if(phaseModel.length > 0){
+    UsedDevice.controlTransfer(phaseModel["type"], phaseModel["request"], phaseModel["value"], phaseModel["index"], phaseModel["data"], function(error, data){
+      if(error){
+        console.log("Error: " + error);
+      }else{
+        
+      }
+      console.log("Data: " + data);
+      phaseModel.shift();
+      trySendData(phaseModel);
+      
+    });
+  }
+  
+}
+
 async function startUSB(){
   const devices = getDeviceList();
 
@@ -16,13 +35,33 @@ async function startUSB(){
       }
   }
 
-  UsedDevice.open();
-  UsedDevice.controlTransfer(bmRequestType(DIRECTION.Out, TYPE.Vendor, RECIPIENT.Device), 0x51, 0, 0, 0x00, function(error, data){
-    if(error){
-      console.log("Error: " + error);
+
+  var initPhases = [
+    {
+      "type" : bmRequestType(DIRECTION.In, TYPE.Vendor, RECIPIENT.Device),
+      "request" : 0x51,
+      "value" : 0,
+      "index" : 0,
+      "data" : Buffer.from([1]),
+    },
+    {
+      "type" : bmRequestType(DIRECTION.Out, TYPE.Vendor, RECIPIENT.Device),
+      "request" : 0x52,
+      "value" : 0,
+      "index" : 2,
+      "data" : Buffer.from("Glare Server\0"),
+    },
+    {
+      "type" : bmRequestType(DIRECTION.Out, TYPE.Vendor, RECIPIENT.Device),
+      "request" : 0x53,
+      "value" : 0,
+      "index" : 2,
+      "data" : 0x00,
     }
-    console.log("Data: " + data);
-  });
+  ]
+  UsedDevice.open();
+  trySendData(initPhases);
+  
 }
 
 module.exports = startUSB;
